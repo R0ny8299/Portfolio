e8359b01e0359d9cc7d8b0ca6f09b0728ec70afa
Rohit Singh <rony.rohitsingh@gmail.com>
2025-04-19 01:09:40 +0530

    redirection changes

--- src/app/component/intro/intro.component.ts
+++ src/app/component/intro/intro.component.ts
@@ -13,7 +13,22 @@ export class IntroComponent implements OnInit {
 
   ngOnInit() {
 
-    this.cvUrl =  ''
+    this.cvUrl =  this.profileService.projects.resumeurl;
   }
-
+ 
+    downloadPdf(): void {
+      const pdfUrl = 'assets/ROHIT-SINGH-2025.pdf';
+    
+      fetch(pdfUrl)
+        .then(response => response.blob())
+        .then(blob => {
+          const url = window.URL.createObjectURL(blob);
+          const a = document.createElement('a');
+          a.href = url;
+          a.download = 'DownloadedFile.pdf';
+          a.click();
+          window.URL.revokeObjectURL(url);
+        });
+    }
+  
 }
